2769a18e473f559c94d467cf3eec0b85ec16a359
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-11-08 21:55:34 +0530

    add DB validation for user registeration

--- src/features/user/userController.ts
+++ src/features/user/userController.ts
@@ -1,13 +1,32 @@
 import { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
+import userModel from './userModel';
 
-export function createUser(req: Request, res: Response, next: NextFunction) {
+export async function createUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const { name, email, password } = req.body;
 
-  // Validation
+  // Input Validation
   if (!name || !email || !password) {
     const error = createHttpError(400, 'All fields are required.');
     return next(error);
+  }
+
+  // DB Validation
+  try {
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const error = createHttpError(
+        400,
+        'User already exists with this email.'
+      );
+      return next(error);
+    }
+  } catch (err) {
+    return next(createHttpError(500, 'Error while getting the error.'));
   }
 
   // Process
